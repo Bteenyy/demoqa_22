c1956da12e19edbb7319d6ea70c246c3959b9860
Boss <rasitsahbutdinov915455@gmail.com>
2023-11-05 13:08:47 +0300

    allure

--- src/test/java/test/MyTestDemoqa22.java
+++ src/test/java/test/MyTestDemoqa22.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.EnteredDataRegComponent;
 
+import static io.qameta.allure.Allure.step;
+
 public class MyTestDemoqa22 extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     EnteredDataRegComponent dataReg = new EnteredDataRegComponent();
@@ -12,31 +14,38 @@ public class MyTestDemoqa22 extends TestBase {
     @Tag("simple")
     @Test
     void demoqaTest() {
-        registrationPage.openPage()
-                .fixBanReg()
-                .setFirstName(dataReg.firstName)
-                .setLastName(dataReg.lastName)
-                .setUserEmail(dataReg.userEmail)
-                .setGender(dataReg.gender)
-                .setUserNumber(dataReg.userNumber)
-                .setDateOfB(dataReg.dayOfB, dataReg.monthCalendar, dataReg.yearCalendar)
-                .setSubjectsInput(dataReg.subjectsInput)
-                .setHobbiesInput(dataReg.hobbiesInput)
-                .setPicturesInput(dataReg.picturesInput)
-                .setAddressInput(dataReg.addressInput)
-                .setStateInput(dataReg.stateInput)
-                .setCityInput(dataReg.cityInput)
-                .finishTest()
-                .checkResult("Student Name", dataReg.firstName + " " + dataReg.lastName)
-                .checkResult("Student Email", dataReg.userEmail)
-                .checkResult("Gender", dataReg.gender)
-                .checkResult("Mobile", dataReg.userNumber)
-                .checkResult("Date of Birth", dataReg.dayOfB + " " + dataReg.monthCalendar + "," + dataReg.yearCalendar)
-                .checkResult("Subjects", dataReg.subjectsInput)
-                .checkResult("Hobbies", dataReg.hobbiesInput)
-                .checkResult("Picture", dataReg.picturesInput)
-                .checkResult("Address", dataReg.addressInput)
-                .checkResult("State and City", dataReg.stateInput + " " + dataReg.cityInput);
-
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .fixBanReg();
+        });
+        step("Fill form", () -> {
+            registrationPage.openPage()
+                    .fixBanReg()
+                    .setFirstName(dataReg.firstName)
+                    .setLastName(dataReg.lastName)
+                    .setUserEmail(dataReg.userEmail)
+                    .setGender(dataReg.gender)
+                    .setUserNumber(dataReg.userNumber)
+                    .setDateOfB(dataReg.dayOfB, dataReg.monthCalendar, dataReg.yearCalendar)
+                    .setSubjectsInput(dataReg.subjectsInput)
+                    .setHobbiesInput(dataReg.hobbiesInput)
+                    .setPicturesInput(dataReg.picturesInput)
+                    .setAddressInput(dataReg.addressInput)
+                    .setStateInput(dataReg.stateInput)
+                    .setCityInput(dataReg.cityInput)
+                    .finishTest();
+        });
+        step("Verify results", () -> {
+            registrationPage.checkResult("Student Name", dataReg.firstName + " " + dataReg.lastName)
+                    .checkResult("Student Email", dataReg.userEmail)
+                    .checkResult("Gender", dataReg.gender)
+                    .checkResult("Mobile", dataReg.userNumber)
+                    .checkResult("Date of Birth", dataReg.dayOfB + " " + dataReg.monthCalendar + "," + dataReg.yearCalendar)
+                    .checkResult("Subjects", dataReg.subjectsInput)
+                    .checkResult("Hobbies", dataReg.hobbiesInput)
+                    .checkResult("Picture", dataReg.picturesInput)
+                    .checkResult("Address", dataReg.addressInput)
+                    .checkResult("State and City", dataReg.stateInput + " " + dataReg.cityInput);
+        });
     }
 }

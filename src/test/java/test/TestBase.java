0ee903ef0673a0c46009640937946936d4bd3768
Boss <rasitsahbutdinov915455@gmail.com>
2023-11-12 11:06:55 +0300

    allure1

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -34,7 +34,6 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-       // Attach.addVideo();
         closeWebDriver();
     }
 }
